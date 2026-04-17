40f01993575e0e20de879b400c4f1d3b3718aa86
Rafaela Mumbach Buenos <rafaela.buenos@totvs.com.br>
2026-04-17 20:27:41 -0300

    hello wrld - primeiro exercicio de ponteiros! ;)

---
+++ ponteiros/p01.c
@@ -0,0 +1,29 @@
+/*
+ * @exercicio.:::::::::::: 01
+ * @conteudo.::::::::::::: Ponteiros
+ * 
+ * @data.::::::::::::::::: 17/04/2026
+ * @responsavel.:::::::::: Rafaela Mumbach
+ * 
+ * @descricao.:::::::::::: Construa uma função que troque o valor de duas variáveis inteiras informadas como parâmetro.
+ */
+
+#include <stdio.h>
+
+void trocar(int *a, int *b) {
+    int temp = *a; //temp=5
+    *a = *b;       //a=10
+    *b = temp;     //b=5
+}
+
+int main() {
+    int x = 5, y = 10;
+
+    printf("Antes da troca: x = %d, y = %d\n", x, y);
+    
+    trocar(&x, &y); //chamada da funcao
+
+    printf("Depois da troca: x = %d, y = %d\n", x, y);
+
+    return 0;
+}
