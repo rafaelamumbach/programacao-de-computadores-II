cacd4ca46710534636d84deac1f78b15ad388bbf
Rafaela Mumbach <rafaela.buenos@totvs.com.br>
2026-08-12 21:25:40 -0300

    atualização: modularização da ma02 + ajustes de inicialização da matriz

--- matrizes/ma02.c
+++ matrizes/ma02.c
@@ -1,21 +1,11 @@
-/* Ler 3 notas para cada um dos 3 alunos de uma turma. Após para cada aluno escreva as suas 3 notas e a média aritmética dessas notas. */
+/* Ler 3 notas para cada um dos 10 alunos de uma turma. Após para cada aluno escreva as suas 3 notas e a média aritmética dessas notas. */
 
 #include <stdio.h>
 
-int main(){
+void informaMedia(float notas[10][3]){
     int i, j;
     float soma=0;
-    float notas[10][3];
-
-    for(i=0; i<5; i++){
-        for(j=0;j<3;j++){
-            printf("Aluno [%d] informe a nota [%d]: ",i+1, j+1);
-            scanf("%f",&notas[i][j]);
-            printf("\n");
-        }
-    }
-
-    for(i=0; i<5; i++){
+    for(i=0; i<10; i++){
         soma = 0;
         printf("Aluno [%d]: ",i+1);
 
@@ -26,4 +16,23 @@ int main(){
         printf("media = %2.1f", soma/3);
         printf("\n");
     }
+}
+
+int main(){
+    int i, j;
+    float soma=0;
+    float notas[10][3] = {
+        {8.5, 4.2, 7.1},
+        {6.3, 9.0, 2.8},
+        {5.5, 1.4, 8.9},
+        {3.7, 7.6, 5.0},
+        {9.8, 6.1, 3.3},
+        {2.5, 8.2, 6.7},
+        {4.9, 1.1, 9.5},
+        {7.3, 5.8, 4.0},
+        {1.9, 8.7, 6.4},
+        {5.2, 3.1, 9.9}
+    };
+
+    informaMedia(notas);
 }
